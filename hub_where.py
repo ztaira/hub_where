864bd65c26937e119ccffa581b742c1496ae751e
ztaira14 <ztaira14@punahou.edu>
2016-10-02 20:22:11 -0400

    Prettify Update 10/2
    Fits with pep8 and pylint now.

--- hub_where.py
+++ hub_where.py
@@ -1,13 +1,16 @@
+"""Script to collect data on Hubway stations."""
+
 import time
 import json
 import requests
-import urllib.request
 
-# get the station information
-# station_info = requests.get('https://api-core.thehubway.com/gbfs/en/station_information.json')
-# station_info = station_info.json()
 
 def setup():
+    """If last_updated.txt does not exist, create the files that will be
+    used to store Hubway station data.
+
+    This makes sure that the text files exist, so writing to them
+    later doesn't throw any errors."""
     try:
         with open('last_updated.txt', 'r') as readfile:
             pass
@@ -23,20 +26,25 @@ def setup():
                 station = shorten_dict(station)
                 workfile.write(json.dumps(station))
 
+
 def get_station_status():
+    """Gets the station status via Hubway's API and returns it as a dict."""
     station_status = requests.get('https://api-core.thehubway.com/gbfs/en/station_status.json')
     station_status = station_status.json()
     return station_status
 
+
 def update_station_logs(station_status):
-    # firstly, log to the last updated file
-    # then, for every station in the station_status dict passed in:
-    # if the last reported time in the file is less than the time in the dict:
-    # write the dict to the file
-    # otherwise do nothing
-    with open('last_updated.txt', 'a') as workfile:
-        workfile.write('\n')
-        workfile.write(str(int(station_status['last_updated'])))
+    """Updates the station logs and last_updated.txt"""
+    # if the file's last updated time < the station's last updated time:
+    # log the station's last updated time to file
+    if int(get_last_line('last_updated.txt')) < int(station_status['last_updated']):
+        with open('last_updated.txt', 'a') as workfile:
+            workfile.write('\n')
+            workfile.write(str(int(station_status['last_updated'])))
+    # for every station in station_status:
+    # if the log's last_reported time < the station's last reported time:
+    # log the station's information to file
     for station in station_status['data']['stations']:
         file_name = station['station_id'] + '.txt'
         last_line = json.loads(get_last_line(file_name))
@@ -46,7 +54,9 @@ def update_station_logs(station_status):
                 station = shorten_dict(station)
                 workfile.write(json.dumps(station))
 
+
 def get_last_line(file_name):
+    """Function to return the last line of a given text file as a string."""
     with open('data/'+file_name, 'rb') as readfile:
         readfile.seek(-2, 2)
         while readfile.read(1) != b'\n':
@@ -54,7 +64,9 @@ def get_last_line(file_name):
         last_line = readfile.readline()
     return last_line.decode("utf-8")
 
+
 def shorten_dict(station):
+    """Function to shorten a station dict's keys so that it takes up less space."""
     station['s_i'] = station.pop('station_id', None)
     station['e_h_a_k'] = station.pop('eightd_has_available_keys', None)
     station['i_ret'] = station.pop('is_returning', None)

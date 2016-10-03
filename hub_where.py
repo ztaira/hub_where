306892dd247b9c8bf0f1fcb7d0489d859684d79e
ztaira14 <ztaira14@punahou.edu>
2016-10-02 20:01:08 -0400

    Shortening Update 10/2
    Shortened the dict keys so that it takes up less space when you save
    them into a text file. Current data rate has taken up 586 KB in the
    last six or so hours. This update will decrease the amount of bytes
    spent on logging dict keys by 70%.

--- hub_where.py
+++ hub_where.py
@@ -18,8 +18,9 @@ def setup():
             workfile.write(str(int(station_status['last_updated'])))
         for station in station_status['data']['stations']:
             file_name = station['station_id'] + '.txt'
-            with open(file_name, 'a') as workfile:
+            with open('data/'+file_name, 'a') as workfile:
                 workfile.write('\n')
+                station = shorten_dict(station)
                 workfile.write(json.dumps(station))
 
 def get_station_status():
@@ -40,17 +41,31 @@ def update_station_logs(station_status):
         file_name = station['station_id'] + '.txt'
         last_line = json.loads(get_last_line(file_name))
         if last_line['last_reported'] < station['last_reported']:
-            with open(file_name, 'a') as workfile:
+            with open('data/'+file_name, 'a') as workfile:
                 workfile.write('\n')
+                station = shorten_dict(station)
                 workfile.write(json.dumps(station))
 
 def get_last_line(file_name):
-    with open(file_name, 'rb') as readfile:
+    with open('data/'+file_name, 'rb') as readfile:
         readfile.seek(-2, 2)
         while readfile.read(1) != b'\n':
             readfile.seek(-2, 1)
         last_line = readfile.readline()
     return last_line.decode("utf-8")
+
+def shorten_dict(station):
+    station['s_i'] = station.pop('station_id', None)
+    station['e_h_a_k'] = station.pop('eightd_has_available_keys', None)
+    station['i_ret'] = station.pop('is_returning', None)
+    station['n_b_d'] = station.pop('num_bikes_disabled', None)
+    station['n_d_d'] = station.pop('num_docks_disabled', None)
+    station['l_r'] = station.pop('last_reported', None)
+    station['i_i'] = station.pop('is_installed', None)
+    station['n_b_a'] = station.pop('num_bikes_available', None)
+    station['n_d_a'] = station.pop('num_docks_available', None)
+    station['i_ren'] = station.pop('is_renting', None)
+    return station
 
 if __name__ == "__main__":
     print("Starting logging!")

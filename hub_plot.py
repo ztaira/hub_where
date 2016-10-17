40f9168315137abeb9b5dd5b8b299def6e9a3932
ztaira14 <ztaira14@punahou.edu>
2016-10-17 10:11:29 -0400

    New Update 10/17
    Working on this week’s data analysis.

--- hub_plot.py
+++ hub_plot.py
@@ -14,11 +14,15 @@ def get_station_info():
 
 def get_station_occupancy_array():
     """Gets how full the stations are over time"""
-    # we want: station number as row, timestamp at column
+    # we want: station number as row, timestamp at column, number of bikes as
+        # content
+    # For example, station_occupancy_array[station_number][timestamp] = 12/15 bikes
+    # I also left debugging comments in here because the array
+        # is uncomfortably large
     time_interval = get_time_interval()
     time_interval = time_interval[1] - time_interval[0]
     time_interval = int(time_interval/10) + 1
-    station_occupancy_array = np.zeros((1, time_interval))
+    station_occupancy_array = np.full((1, time_interval), 2)
     # cumulative_size = 0
     # print("Shape of master:", station_occupancy_array.shape)
     for station_number in range(1, 219):
@@ -47,7 +51,6 @@ def get_single_station_occupancy(station_number, array_length):
                     value = line['n_b_a'] / (line['n_b_a'] + line['n_d_a'])
                 if (index < 0):
                     index = 0
-                # print("Line, index, value:", line_counter, index, value)
                 single_station_occupancy_array[0][index] = value
                 line_counter += 1
     except FileNotFoundError:
@@ -57,6 +60,51 @@ def get_single_station_occupancy(station_number, array_length):
             single_station_occupancy_array[0][index] = single_station_occupancy_array[0][index-1]
     print("Returned data for station:", station_number, "\n")
     return single_station_occupancy_array
+
+def get_file_length_array():
+    """Gets an array containing all the station lengths"""
+    file_length_array = []
+    for station_number in range(219):
+        file_length_array.append(get_file_length(station_number))
+    for item in reversed(file_length_array):
+        if item == -1:
+            file_length_array.pop(item)
+    return file_length_array
+
+
+def get_file_length(station_number):
+    """Gets the length of a station's data file"""
+    file_name = 'data/' + str(station_number) + '.txt'
+    line_number = 0
+    try:
+        with open(file_name, 'r') as readfile:
+            for line in readfile:
+                line_number += 1
+    except FileNotFoundError:
+        line_number = -1
+    return line_number
+
+def get_station_averages(station_occupancy_array):
+    """Gets how full the stations are on average"""
+    average_list = []
+    cumulative_total = 0
+    num_entries = 0
+    for station in station_occupancy array:
+        for item in station:
+            print(item)
+            if item <= 1:
+                cumulative_total += item
+                num_entries += 1
+            else:
+                cumulative_total += 0
+        average = cumulative_total / num_entries
+        cumulative_total = 0
+        num_entries = 0
+        average_list.append(average)
+    for item in reversed(average_list):
+        if item == -1:
+            average_list.pop(item)
+    return average_list
 
 def get_time_interval():
     """Function to get the time interval where data was collected from
@@ -84,15 +132,24 @@ def parse_station_coordinates(station_info):
     else:
         return None
 
-def plot_station_locations(station_coords):
+def plot_station_locations_and_activity(station_coords, station_activity):
     """Takes a list of station coordinates and plots it"""
     plt.figure()
-    plt.scatter(station_coords[1], station_coords[2])
+    plt.scatter(station_coords[2], station_coords[1], s=station_activity)
+    plt.title("Hubway Stations by GPS Coordinates and Activity Level")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
 
 if __name__ == "__main__":
-    # station_info = get_station_info()
-    # station_coords = parse_station_coordinates(station_info)
-    # plot_station_locations(station_coords)
+    # To plot station data via GPS coordinates and station activity
+    station_info = get_station_info()
+    station_activity = get_file_length_array()
+    station_coords = parse_station_coordinates(station_info)
+    for item in station_coords:
+        print(item)
+    plot_station_locations_and_activity(station_coords, station_activity)
+
     station_occupancy_array = get_station_occupancy_array()
-    print("Shape of final:", station_occupancy_array.shape)
+    get_station_averages(station_occupancy_array)
